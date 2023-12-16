86fa936800f57a0123a49ad29c2c7cd0aa4127e4
= <=>
2023-12-17 01:36:26 +0800

    :sparkles: feat: 实现了安全管理器

---
+++ src/main/java/com/yupi/yuojcodesandbox/security/DenySecurityManager.java
@@ -0,0 +1,16 @@
+package com.yupi.yuojcodesandbox.security;
+
+import java.security.Permission;
+
+/**
+ * 默认安全管理器
+ */
+public class DenySecurityManager extends SecurityManager {
+
+    // 检查所有的权限
+    @Override
+    public void checkPermission(Permission perm) {
+        throw new SecurityException("权限异常：" + perm.toString());
+    }
+
+}
