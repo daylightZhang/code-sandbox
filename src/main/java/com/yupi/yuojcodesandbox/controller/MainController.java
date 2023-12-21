5d37e0c9958826971583ebd7bf0c4b2c582faede
= <=>
2023-12-21 22:44:29 +0800

    :sparkles: feat: 实现接口

--- src/main/java/com/yupi/yuojcodesandbox/controller/MainController.java
+++ src/main/java/com/yupi/yuojcodesandbox/controller/MainController.java
@@ -1,12 +1,51 @@
 package com.yupi.yuojcodesandbox.controller;
 
+import com.yupi.yuojcodesandbox.JavaNativeCodeSandbox;
+import com.yupi.yuojcodesandbox.model.ExecuteCodeRequest;
+import com.yupi.yuojcodesandbox.model.ExecuteCodeResponse;
+import jakarta.annotation.Resource;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/")
 public class MainController {
+    // 定义鉴权请求头和密钥
+    private static final String AUTH_REQUEST_HEADER = "auth";
+
+    private static final String AUTH_REQUEST_SECRET = "secretKey";
+
+    @Resource
+    private JavaNativeCodeSandbox javaNativeCodeSandbox;
+
+
     @GetMapping("/health")
     public String healthCheck() {
         return "OK";
     }
+
+    /**
+     * 执行代码
+     *
+     * @param executeCodeRequest
+     * @return
+     */
+    @PostMapping("/executeCode")
+    ExecuteCodeResponse executeCode(@RequestBody ExecuteCodeRequest executeCodeRequest, HttpServletRequest request,
+                                    HttpServletResponse response) {
+        // 基本的认证
+        String authHeader = request.getHeader(AUTH_REQUEST_HEADER);
+        if (!AUTH_REQUEST_SECRET.equals(authHeader)) {
+            response.setStatus(403);
+            return null;
+        }
+        if (executeCodeRequest == null) {
+            throw new RuntimeException("请求参数为空");
+        }
+        return javaNativeCodeSandbox.executeCode(executeCodeRequest);
+    }
+
 }
